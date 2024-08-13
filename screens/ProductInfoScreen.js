a5a529e2461ced9488c64a60ddcb5e484c5a130a
Govind Singh <govind.singh@skit.ai>
2024-08-13 21:16:38 +0530

    current transactions page

--- screens/ProductInfoScreen.js
+++ screens/ProductInfoScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -9,43 +9,112 @@ import {
   KeyboardAvoidingView,
   Platform,
   SafeAreaView,
+  Image
 } from 'react-native';
-import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { AntDesign, Ionicons, MaterialIcons} from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { app as firebase , database } from '../config/firebase'; // Adjust this import based on your Firebase setup
+// import { useAuth } from '../context/AuthContext'; // Assume you have an auth context
+import { collection, addDoc,setDoc, doc, query, where, orderBy, onSnapshot, serverTimestamp } from "firebase/firestore";
+import { useSelector } from 'react-redux';
 
-const TransactionListScreen = () => {
+const TransactionListScreen = ({ route }) => {
   const navigation = useNavigation();
   const [transactions, setTransactions] = useState([]);
-  const [newExpense, setNewExpense] = useState('');
-  const [newNote, setNewNote] = useState('');
+  const [newAmount, setNewAmount] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const { groupId, groupName } = route.params;
+  // const { user } = useAuth(); // Assume this hook provides the current user
+  const userData = useSelector((state) => state.auth.user);
+  const userId = userData.userId;
 
-  const addTransaction = () => {
-    if (newExpense.trim() !== '' && newNote.trim() !== '') {
-      setTransactions([
-        ...transactions,
-        { 
-          id: Date.now().toString(), 
-          amount: newExpense, 
-          note: newNote,
-          date: new Date() 
-        },
-      ]);
-      setNewExpense('');
-      setNewNote('');
+  useEffect(() => {
+    if (!groupId) return;
+
+    const transactionsRef = collection(database, 'transactions');
+    const q = query(transactionsRef, where('groupId', '==', groupId), orderBy('createdAt', 'desc'));
+    
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const fetchedTransactions = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setTransactions(fetchedTransactions);
+    });
+    return () => unsubscribe();
+  }, [groupId]);
+
+  const addTransaction = async () => {
+    if (newAmount.trim() !== '' && newDescription.trim() !== '') {
+      const timestamp = Date.now(); // Get the current timestamp in milliseconds
+      const transactionId = `${userId}_${timestamp}`; // Generate the custom transaction ID
+
+      const transactionData = {
+        transactionId: transactionId,
+        type: 'expense',
+        createdBy: userData.displayName, // Use the user ID from props or state
+        amount: parseFloat(newAmount),
+        description: newDescription,
+        category: 'General', // You might want to add a category input
+        date: new Date().toISOString(),
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        groupId: groupId,
+        userId
+      };
+
+      try {
+        const transactionsRef = collection(database, 'transactions');
+        const docRef = doc(transactionsRef, transactionId);
+        await setDoc(docRef, transactionData);
+        setNewAmount('');
+        setNewDescription('');
+      } catch (error) {
+        console.error("Error adding transaction: ", error);
+      }
     }
   };
 
-  const renderTransaction = ({ item }) => (
-    <View style={styles.transactionItem}>
-      <View style={styles.transactionContent}>
-        <Text style={styles.transactionAmount}>₹{item.amount}</Text>
-        <Text style={styles.transactionNote}>{item.note}</Text>
-        <Text style={styles.transactionDate}>
-          {item.date.toLocaleString()}
-        </Text>
-      </View>
-    </View>
+  const goToGroupInfo = () => {
+    navigation.navigate('GroupInfoScreen', { groupId, groupName, transactions });
+  };
+
+  const renderHeader = () => (
+    <Pressable style={styles.header}>
+      <AntDesign onPress={() => navigation.goBack()} name="arrowleft" size={24} color="white" style={styles.backButton} />
+      <Pressable onPress={goToGroupInfo}  style={styles.header_2}>
+        <Image style={styles.groupImage} source={{ uri: 'https://picsum.photos/200' }} />
+        <View style={styles.headerInfo}>
+          <Text style={styles.groupName}>{groupName}</Text>
+          <Text style={styles.memberCount}>5 members</Text>
+        </View>
+      </Pressable>
+    </Pressable>
   );
+
+
+  const renderTransaction = ({ item }) => {
+    const isCurrentUserTransaction = item.userId === userId;//user.uid;
+    return (
+      <View style={[
+        styles.transactionItem,
+        isCurrentUserTransaction ? styles.currentUserTransaction : styles.otherUserTransaction
+      ]}>
+        <View style={[
+          styles.transactionContent,
+          isCurrentUserTransaction ? styles.currentUserContent : styles.otherUserContent
+        ]}>
+          <Text style={styles.transactionAmount}>₹{item.amount}</Text>
+          <Text style={styles.transactionNote}>{item.description}</Text>
+          <Text style={styles.transactionNote}>{item.createdBy}</Text>
+          <Text style={styles.transactionDate}>
+          {/* {item.createdAt ? item.createdAt.toDate().toLocaleString() : 'Date not available'} */}
+          {item.createdAt}
+          </Text>
+        </View>
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -53,34 +122,29 @@ const TransactionListScreen = () => {
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.container}
       >
-        <View style={styles.header}>
-          <Pressable onPress={() => navigation.goBack()}>
-            <AntDesign name="arrowleft" size={24} color="white" />
-          </Pressable>
-          <Text style={styles.headerTitle}>Expense Group</Text>
-        </View>
+        {renderHeader()}
 
         <FlatList
           data={transactions}
           renderItem={renderTransaction}
           keyExtractor={(item) => item.id}
           style={styles.list}
-          // inverted
+          inverted
         />
 
         <View style={styles.inputContainer}>
           <TextInput
             style={styles.amountInput}
             placeholder="Amount"
-            value={newExpense}
-            onChangeText={setNewExpense}
+            value={newAmount}
+            onChangeText={setNewAmount}
             keyboardType="numeric"
           />
           <TextInput
             style={styles.noteInput}
-            placeholder="Add a note"
-            value={newNote}
-            onChangeText={setNewNote}
+            placeholder="Add a description"
+            value={newDescription}
+            onChangeText={setNewDescription}
           />
           <Pressable onPress={addTransaction} style={styles.sendButton}>
             <Ionicons name="send" size={24} color="white" />
@@ -105,6 +169,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 15,
     backgroundColor: '#075E54',
+  },
+  header_2:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  backButton: {
+    marginRight: 10,
+  },
+  groupImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 15,
+  },
+  headerInfo: {
+    flex: 1,
+  },
+  groupName: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  memberCount: {
+    color: 'white',
+    fontSize: 14,
   },
   headerTitle: {
     color: 'white',
@@ -168,6 +257,18 @@ const styles = StyleSheet.create({
     height: 50,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  currentUserTransaction: {
+    justifyContent: 'flex-end',
+  },
+  otherUserTransaction: {
+    justifyContent: 'flex-start',
+  },
+  currentUserContent: {
+    backgroundColor: '#DCF8C6',
+  },
+  otherUserContent: {
+    backgroundColor: 'white',
   },
 });
 
